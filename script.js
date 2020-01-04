b6cf2488f6324287a5c75caac29b39569a6e74fb
Isabella Karlsgaard <isabellakarlsgaard@Isabellas-MacBook-Pro.local>
2020-01-04 18:34:21 +0100

    nyt logo - begyndende på om side

---
+++ script.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", sidenVises);
+
+
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
+}
+
+
+function logoClick() {
+    console.log("logoclick");
+    document.querySelector("#logo").addEventListener("click", visForside);
+}
+
+function visForside() {
+    console.log("visForside");
+
+
+
+}
